05ebe6d12e33337377764734cfb05195ec8fedec
dwarms <darcywarms@usf.edu>
2026-08-26 10:42:46 -0400

    Add main.py via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#######################################
+# Darcy Warms # darcywarms@usf.edu    #
+#-------------------------------------#
+# main.py: prints "Hello, World!" and #
+# examines behavior of data types.    #
+#######################################
+
+""" we want this script to PRINT the string Hello, World!
+"""
+print("Hello, World!")
+
+a = "hello world!"
+print(a)
